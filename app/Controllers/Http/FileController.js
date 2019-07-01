a6b314b323226d3512bc27cb648346d3ce86d2ef
Ricardo Colares <ricardo.colares@osf-global.com>
2019-07-01 09:59:16 -0300

    first commit

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const File = use('App/models/File')
+const Helpers = use('Helpers')
+
+class FileController {
+  /**
+   * Create/save a new file.
+   * POST files
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {}
+}
+
+module.exports = FileController
